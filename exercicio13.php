0aa983fe04f50fbf7615ad3f5e8620fe8ba0c318
Gui <guilhermerafaell10@gmail.com>
2026-08-14 15:22:52 -0300

    ex13 finalizado

---
+++ exercicio13.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>EXERCÍCIO 13 - QUILÔMETROS PARA MILHAS</h1>
+    <form action="resultado13.php" method="post">
+        <label for="km">Insira o valor em quilômetros: </label>
+        <input type="number" name="km" id="km"><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ resultado13.php
@@ -0,0 +1,19 @@
+<?php
+$km = $_POST["km"];
+$milhas = $km * 0.621371;
+echo "$km km equivale a $milhas milhas";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <a href="index.php">RETORNAR AO INÍCIO</a>
+</body>
+</html>
